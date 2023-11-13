a97ee265ade259d7d30cc97a06123699c6073c46
Corey Minyard <minyard@acm.org>
2023-11-13 13:55:49 -0600

    addr: Add some missing constants
    Signed-off-by: Corey Minyard <minyard@acm.org>

--- src/addr.rs
+++ src/addr.rs
@@ -8,6 +8,17 @@ pub struct Addr {
     ai: *const raw::gensio_addr,
 }
 
+pub const GENSIO_NET_PROTOCOL_TCP: i32 = 1;
+pub const GENSIO_NET_PROTOCOL_UDP: i32 = 2;
+pub const GENSIO_NET_PROTOCOL_SCTP: i32 = 3;
+pub const GENSIO_NET_PROTOCOL_UNIX: i32 = 4;
+
+pub const GENSIO_NETTYPE_UNSPEC: i32 = 0;
+pub const GENSIO_NETTYPE_IPV4: i32 = 1;
+pub const GENSIO_NETTYPE_IPV6: i32 = 2;
+pub const GENSIO_NETTYPE_UNIX: i32 = 3;
+pub const GENSIO_NETTYPE_AX25: i32 = 4;
+
 pub fn new(ai: *const raw::gensio_addr) -> Addr {
     Addr { ai: ai }
 }
